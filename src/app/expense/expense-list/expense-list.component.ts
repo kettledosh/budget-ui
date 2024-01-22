87ddea797738e922d20d03a98f0f2c5f37862f6b
kettledosh <104758239+kettledosh@users.noreply.github.com>
2024-01-22 14:40:50 +0100

    Mockup expense-Modal

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { addMonths, set } from 'date-fns';
 import { ModalController } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
-import {Expense, SortOption} from '../../shared/domain';
+import {Category, Expense, SortOption} from '../../shared/domain';
+import {CategoryService} from "../../category/category.service";
 import {FormGroup} from "@angular/forms";
+import {ToastService} from "../../shared/service/toast.service";
 
 @Component({
   selector: 'app-expense-overview',
@@ -11,7 +13,11 @@ import {FormGroup} from "@angular/forms";
 })
 export class ExpenseListComponent {
   date = set(new Date(), { date: 1 });
-  readonly searchForm: FormGroup;
+  categories: Category[] = [];
+
+
+
+
   readonly sortOptions: SortOption[] = [
     { label: 'Created at (newest first)', value: 'createdAt,desc' },
     { label: 'Created at (oldest first)', value: 'createdAt,asc' },
@@ -20,12 +26,24 @@ export class ExpenseListComponent {
     { label: 'Name (A-Z)', value: 'name,asc' },
     { label: 'Name (Z-A)', value: 'name,desc' },
   ];
-  constructor(private readonly modalCtrl: ModalController) {}
+  constructor(private readonly modalCtrl: ModalController,
+              private readonly categoryService: CategoryService,
+              private readonly toastService: ToastService,
+
+              ) {}
 
   addMonths = (number: number): void => {
     this.date = addMonths(this.date, number);
   };
-
+  ionViewWillEnter(): void {
+    this.loadAllCategories();
+  }
+  private loadAllCategories(): void {
+    this.categoryService.getAllCategories({ sort: 'name,asc' }).subscribe({
+      next: (categories) => (this.categories = categories),
+      error: (error) => this.toastService.displayErrorToast('Could not load categories', error),
+    });
+  }
   async openModal(expense?: Expense): Promise<void> {
     const modal = await this.modalCtrl.create({
       component: ExpenseModalComponent,
